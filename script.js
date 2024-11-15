564906449da7179a75dd8e8d93b4ed2243e35b8c
Eloi RM <eloimailmaynad@gmail.com>
2024-11-15 12:06:01 +0100

    fix typo et ajout commentaires

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const latinInput = document.querySelector('.input-latin')
 const morseInput = document.querySelector('.input-morse')
 //-----------------------
 
-function getLatinCharacterList(myString){
+function getLatinCharacterList(myString){ //je découpe mon texte
     const myArray = myString.split('')
     return myArray
 }
 
-function translateLatinCharacter(character){
+function translateLatinCharacter(character){ //je traduis un caractère
     let myCharacter = character.toUpperCase()
-    for(const letter in latinToMorse){
+    for(const letter in latinToMorse){ //rappel latinToMorse est mon dictionnaire annexe
         if(myCharacter === letter){
             myCharacter = latinToMorse[letter]
         }
@@ -22,54 +22,54 @@ function translateLatinCharacter(character){
     return myCharacter
 }
 
-function encode(myString){
-    const myText = getLatinCharacterList(myString)
+function encode(myString){ //j'appelle mes fonctions pour retourner un texte encodé
+    const mySplittedText = getLatinCharacterList(myString)
     let encodedText = ""
-    for (let i = 0; i < myText.length; i++){
-        let encodedCharacter = translateLatinCharacter(myText[i])
-        if (encodedCharacter === " "){
+    for (let i = 0; i < mySplittedText.length; i++){ //je traduis chaque caractère de mon tableau
+        let encodedCharacter = translateLatinCharacter(mySplittedText[i])
+        if (encodedCharacter === " "){ //pas dans la fonction traduction car ce n'est pas de la traduction
             encodedCharacter = "/"
         }
-        encodedText += encodedCharacter + " "
+        encodedText += encodedCharacter + " " //j'ajoute le caractère traduit au texte avec un espace pour la lisibilité 
     }
     return encodedText
 }
 
-function getMorseCharacterList(myString){
+function getMorseCharacterList(myString){ //je découpe mon texte
     const myArray = myString.split(' ')
     return myArray
 }
 
-function translateMorseCharacter(character){
+function translateMorseCharacter(character){ //je traduis un caractère
     let myCharacter = character
-    for(const morseLetter in morseToLatin){
+    for(const morseLetter in morseToLatin){ //rappel morseToLatin est mon dictionnaire annexe
         if(myCharacter === morseLetter){
             myCharacter = morseToLatin[morseLetter]
         }
     }
-    myCharacter = myCharacter.toLowerCase()
+    myCharacter = myCharacter.toLowerCase() //bonus pour la forme du texte final
     return myCharacter
 }
 
-function decode(myString){
-    const myText = getMorseCharacterList(myString)
+function decode(myString){ //j'appelle mes fonctions pour retourner un texte décodé
+    const mySplittedText = getMorseCharacterList(myString)
     let decodedText = ""
-    for (let i = 0; i < myText.length; i++){
-        let decodedCharacter = translateMorseCharacter(myText[i])
-        if (decodedCharacter === "/"){
+    for (let i = 0; i < mySplittedText.length; i++){ //je traduis chaque caractère de mon tableau
+        let decodedCharacter = translateMorseCharacter(mySplittedText[i])
+        if (decodedCharacter === "/"){ //pas dans la fonction traduction car ce n'est pas de la traduction
             decodedCharacter = " "
         }
-        decodedText += decodedCharacter
+        decodedText += decodedCharacter //j'ajoute le caractère traduit au texte
     }
     return decodedText
 }
 
-buttonEncode.addEventListener('click', ()=>{
+buttonEncode.addEventListener('click', ()=>{ //execute le code quand on clique dessus
     const text = encode(latinInput.value)
     morseInput.value = text
 })
 
-buttonDecode.addEventListener('click', ()=>{
+buttonDecode.addEventListener('click', ()=>{ //execute le code quand on clique dessus
     const text = decode(morseInput.value)
     latinInput.value = text
 })
